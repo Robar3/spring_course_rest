4d05294aefccda284a2fd449557fc50473454b0d
Иван <terehov94@mail.ru>
2021-09-29 18:30:33 +0300

    Project to learn spring rest

---
+++ src/main/java/com/zaurtregulov/spring/rest/exeption_handling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.zaurtregulov.spring.rest.exeption_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> HandleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData>HandleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
